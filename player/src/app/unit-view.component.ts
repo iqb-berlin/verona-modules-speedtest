03a0f877f9573943a95298328eb3078bf16f6972
rhenck <richard.henck@iqb.hu-berlin.de>
2024-04-15 17:38:25 +0200

    [player] Improve layout and add alignment support

--- player/src/app/unit-view.component.ts
+++ player/src/app/unit-view.component.ts
@@ -14,15 +14,17 @@ import { MatIcon } from '@angular/material/icon';
     NgIf
   ],
   template: `
-    <div class="question">
-      <img *ngIf="question.imgSrc" [src]="question.imgSrc">
-      {{ question.text }}
-    </div>
-    <div class="answers">
-      <button mat-raised-button *ngFor="let answer of question.anwers; let i = index;"
-              (click)="responseGiven.emit(i)">
-        {{ answer.text }}
-      </button>
+    <div class="wrapper" [style.flex-direction]="question.layout">
+      <div class="question" [style.margin]="question.layout == 'column' ? '0 15%' : '0 5%'">
+        <img *ngIf="question.imgSrc" [src]="question.imgSrc">
+        <p>{{ question.text }}</p>
+      </div>
+      <div class="answers" [style.flex-direction]="question.layout == 'row' ? 'column' : 'row'">
+        <button mat-raised-button *ngFor="let answer of question.anwers; let i = index;"
+                (click)="responseGiven.emit(i)">
+          {{ answer.text }}
+        </button>
+      </div>
     </div>
   `,
   styleUrls: ['unit-view.component.css']
