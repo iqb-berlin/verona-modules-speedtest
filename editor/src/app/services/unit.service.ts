116ab5e062d6dbf21be33bb75b66fae744cba0d2
rhenck <richard.henck@iqb.hu-berlin.de>
2024-05-06 11:40:47 +0200

    [editor] Use central unit def version from package.json

--- editor/src/app/services/unit.service.ts
+++ editor/src/app/services/unit.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import packageInfo from 'packageInfo';
 import { Unit } from 'common/interfaces/unit';
 import { FileService } from 'common/services/file.service';
 import { VeronaAPIService } from './verona-api.service';
@@ -7,9 +8,10 @@ import { VeronaAPIService } from './verona-api.service';
   providedIn: 'root'
 })
 export class UnitService {
+  unitDefVersion = packageInfo.config.unit_definition_version;
   unit: Unit = {
     type: 'speedtest-unit-defintion',
-    version: '0.2',
+    version: this.unitDefVersion,
     questions: [],
     layout: 'column'
   };
@@ -17,7 +19,7 @@ export class UnitService {
   constructor(private veronaApiService: VeronaAPIService) { }
 
   loadUnitDefinition(unitDefinition: string): void {
-    this.unit = JSON.parse(unitDefinition);
+    if (unitDefinition) this.unit = JSON.parse(unitDefinition);
   }
 
   saveUnitToFile(): void {
@@ -39,7 +41,7 @@ export class UnitService {
       });
     this.unit = {
       type: 'speedtest-unit-defintion',
-      version: '0.1',
+      version: this.unitDefVersion,
       layout: 'column',
       questions: newQuestions
     };
