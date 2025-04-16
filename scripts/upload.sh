616c530fdd6ba44edd0ecc26943ea4d8162e2942
rhenck <richard.henck@iqb.hu-berlin.de>
2025-04-16 16:28:27 +0200

    Add script to upload modules into IQB studio

---
+++ scripts/upload.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <filepath>"
+    exit 1
+fi
+
+if [ -z "$IQB_STUDIO_PW" ]; then
+    read -sp "Enter password: " IQB_STUDIO_PW
+fi
+
+
+SERVER="example.com"
+REMOTE_PATH="/upload/"
+
+# Login
+TOKEN=$(curl --silent --show-error --header "Content-Type: application/json" --header "app-version: 12.1.1" \
+--request POST \
+--data "{\"username\": \"richard\", \"password\": \"$IQB_STUDIO_PW\"}" \
+https://www.iqb-studio.de/api/login)
+
+# Remove quotes from token
+TOKEN=${TOKEN:1:-1}
+
+# Upload file
+curl --header "Authorization: Bearer $TOKEN" \
+--request POST \
+--form "file=@$1" \
+https://www.iqb-studio.de/api/admin/verona-modules
