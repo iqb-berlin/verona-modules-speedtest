06cdef83f50fb5af34021d03d33309aec2f60ffe
rhenck <richard.henck@iqb.hu-berlin.de>
2025-05-16 16:27:38 +0200

    Fix build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,20 +8,18 @@ fi
 #rm -rf .angular/cache
 
 ### Build Angular project ###
-cd $1
-ng build --output-hashing=none
-cd ..
+ng build --project $1 --output-hashing=none
 
 # Pack JS and CSS
-node node_modules/iqb-dev-components/src/js_css_packer.js $1/dist $1 $1/dist
+node node_modules/iqb-dev-components/src/js_css_packer.js dist $1 dist
 
 # Use prepared HTML
-cp $1/index-prod.html $1/dist/index.html
+cp projects/$1/src/index-prod.html dist/index.html
 
 # Set correct version to JSON description
-sed -i -e 's/version-placeholder/'${2}'/g' $1/dist/index.html
+sed -i -e 's/version-placeholder/'${2}'/g' dist/index.html
 
 # Pack dist
-node scripts/distpacker.js $1/dist iqb-$1-speedtest-$2.html $1
-
-cp $1/dist/iqb-$1-speedtest-$2.html dist/
+node scripts/distpacker.js dist iqb-$1-speedtest-$2.html $1
+#
+#cp dist/iqb-$1-speedtest-$2.html dist/
